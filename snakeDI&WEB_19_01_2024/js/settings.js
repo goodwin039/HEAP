a444b1b17ff0398acb0a907906f532fef5d9b474
Andrew Ermolaev <goodwin039@gmail.com>
2024-01-19 11:44:57 +0300

    19-01-2024
    Snake is moving

---
+++ snakeDI&WEB_19_01_2024/js/settings.js
@@ -0,0 +1,11 @@
+let width, height, row, ceil, colors;
+
+width = 600;
+height = 600;
+ceil = 30;
+row = width / ceil;
+
+colors = {
+    snakeHead: "#ffffff",
+    snakeBody: "#1fb9dd"
+};
---
+++ snakeDI&WEB_19_01_2024/js/state.js
@@ -0,0 +1,11 @@
+const state = {
+    snake:{
+        tail:[
+            {x: 1, y: 1, d:"right", h: false},
+            {x: 2, y: 1, d:"right", h: false},
+            {x: 3, y: 1, d:"right", h: false},
+            {x: 4, y: 1, d:"right", h: true},
+        ],
+        direction: "right"
+    }
+};
